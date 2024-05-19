2087994556612759b0851284420dba4fd275634e
miriampicc <miriam.piccinini00@gmail.com>
2024-05-19 14:45:58 +0100

    magnetisation correction DA

--- data analysis/Magnetization.py
+++ data analysis/Magnetization.py
@@ -78,8 +78,8 @@ for l in L:
         std1=0
         std2=0
 
-        #file_path = f"/home/x_mirpi/Output_TBG/K_{K}_first/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/Single_Magnetization.txt'
-        file_path = f"/Users/mirimi/Desktop/OUTPUT_cluster/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/Single_Magnetization.txt'
+        file_path = f"/home/x_mirpi/Output_TBG/K_{K}/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/Single_Magnetization.txt'
+        #file_path = f"/Users/mirimi/Desktop/OUTPUT_cluster/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/Single_Magnetization.txt'
 
 
         with open(file_path, 'r') as file:
