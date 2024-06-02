a2304b2bf426520437a8fceb298b702108a979dd
miriampicc <miriam.piccinini00@gmail.com>
2024-06-02 18:54:55 +0200

    helicity sum inf

--- data analysis/Js_infinity.py
+++ data analysis/Js_infinity.py
@@ -169,6 +169,6 @@ plt.ylabel(r'$J_s$')
 plt.title(f' $K={K}$ e = {e} $L_0 = {L0}$')
 plt.legend()
 plt.grid(True)
-plt.savefig(f'Helicity_sum2_K{K}_e{e}_L0{L0}.jpg')
+plt.savefig(f'Js_inf_K{K}_e{e}_L0{L0}.jpg')
 
 plt.show()
