5795e34fc50ed6a6bb865878c0099be4993f3373
miriampicc <miriam.piccinini00@gmail.com>
2024-06-17 15:07:00 +0200

    histogram plot4

--- data analysis/Histogram.py
+++ data analysis/Histogram.py
@@ -90,7 +90,7 @@ for l in L:
     N = l * l
 
     #file_path = f"/Users/mirimi/Desktop/OUTPUT_cluster/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/Energy.txt'
-    file_path = f"/home/x_mirpi/Output_TBG/K_{K}/e_{e}/L{L}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{beta}" + '/Energy.txt'
+    file_path = f"/home/x_mirpi/Output_TBG/K_{K}/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{beta}" + '/Energy.txt'
     #print(n)
 
     with open(file_path, 'r') as file:
