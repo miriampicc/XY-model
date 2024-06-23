d7c579a04087bee29e0378de88184c6e688d4ac5
miriampicc <miriam.piccinini00@gmail.com>
2024-06-23 13:32:02 +0200

    links 3

--- data analysis/Binder_Cumulant_beta.py
+++ data analysis/Binder_Cumulant_beta.py
@@ -97,7 +97,7 @@ for l in L:
         temperatures.append(t)
         betas.append(bb)
 
-        file_path = f"/home/x_mirpi/Output_TBG/K_{K}_tdf/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}_tdf/beta_{n}" + '/trsb_magnetization.txt'
+        file_path = f"/home/x_mirpi/Output_TBG/K_{K}_tdf/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/trsb_magnetization.txt'
 
         #print(n)
 
