73f5c8122f0775536d072938acf4d579e1ad9413
miriampicc <miriam.piccinini00@gmail.com>
2024-06-27 09:54:43 +0200

    link Js inifinity

--- data analysis/Js_infinity.py
+++ data analysis/Js_infinity.py
@@ -107,7 +107,7 @@ for l in L:
         Jd2 = []
         Ic2 = []
 
-        file_path1 = f"/home/x_mirpi/Output_TBG/K_{K}_tdf2/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}"+ '/Helicity_modulus1.txt'
+        file_path1 = f"/home/x_mirpi/Output_TBG/K_{K}_tdf/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}"+ '/Helicity_modulus1.txt'
 
         with open(file_path1, "r") as file:
             for line in file:
