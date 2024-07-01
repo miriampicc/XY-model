4af48843912933d32dbb85b1c983725fabc79aab
miriampicc <miriam.piccinini00@gmail.com>
2024-07-01 21:36:42 +0200

    molt. superfluidity2

--- data analysis/Multiple_superfluidity.py
+++ data analysis/Multiple_superfluidity.py
@@ -100,7 +100,7 @@ for l in L :
                     Ic1.append(float(columns[1]))
 
         
-        file_path2 = f"/home/x_mirpi/Output_TBG/K2_{K}/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/Helicity_modulus2.txt'
+        file_path2 = f"/home/x_mirpi/Output_TBG/K_{K}_tdf/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/Helicity_modulus2.txt'
 
         with open(file_path2, "r") as file:
             for line in file:
