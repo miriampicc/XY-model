5da79c750016673512f536785980c97019636c8e
miriampicc <miriam.piccinini00@gmail.com>
2024-07-01 12:08:42 +0200

    binder cumulant vs beta modified

--- data analysis/Binder_Cumulant_beta.py
+++ data analysis/Binder_Cumulant_beta.py
@@ -1,5 +1,4 @@
-import numpy as np 
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
@@ -33,23 +32,7 @@ print("beta low=", beta_low)
 print("rank=", rank)
 
 
-colors = [
-    "#00008B",  # Dark Blue
-    "#0000CD",  # Medium Blue
-    "#4169E1",  # Royal Blue
-    "#1E90FF",  # Dodger Blue
-    "#00BFFF",  # Deep Sky Blue
-    "#87CEEB",  # Sky Blue
-    "#87CEFA",  # Light Sky Blue
-    "#EEE8AA",  # Pale Goldenrod
-    "#F0E68C",  # Khaki
-    "#FFD700",  # Gold
-    "#F08080",  # Light Coral
-    "#FF6347"   # Tomato
-]
-
-
-def calculate_std(data): 
+def calculate_std(data):
     try:
         std_deviation = np.std(data)
         std_deviation = std_deviation / (np.sqrt(len(data)-1))
@@ -59,73 +42,82 @@ def calculate_std(data):
         print(f"Error: {e}")
         return None
 
-def calculate_mean(data): 
-    mean = sum(data) / len(data)
+def calculate_mean(data):
+    mean = np.mean(data)
     return mean
 
 
-#let us obtain the temperatures 
+colors = [
+    "#00008B",  # Dark Blue
+    "#0000CD",  # Medium Blue
+    "#1E90FF",  # Dodger Blue
+    "#87CEEB",  # Sky Blue
+    "#EEE8AA",  # Pale Goldenrod
+    "#F0E68C",  # Khaki
+    "#FFD700",  # Gold
+    "#F08080",  # Light Coral
+    "#FF6347"   # Tomato
+]
+
+
+# Let us obtain the temperatures
 temperatures = []
 delta = (1/beta_low - 1/beta_high)/(rank)
 T_high = 1/beta_low
 T_low = 1/beta_high
-#print(delta)
 
 delta_beta = (beta_high - beta_low)/(rank)
 
-i=0
+i = 0
 
 for l in L:
-    
-    N= l * l
-
     pseudo_magn = []
-    mm_2_values = []
-    mm_4_values = [] 
     cumulant = []
     temperatures = []
-
     betas = []
 
-    for n in range(rank) : 
-
+    for n in range(rank):
         t = T_high - n * delta
-        print (t)
-
-        bb = beta_low + delta_beta * n 
-        
         temperatures.append(t)
+
+        bb = beta_low + delta_beta * n
         betas.append(bb)
 
-        file_path = f"/home/x_mirpi/Output_TBG/K_{K}/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/trsb_magnetization.txt'
+        file_path = f"/home/x_mirpi/Output_TBG/K_{K}/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}/trsb_magnetization.txt"
 
-        #print(n)
+        try:
+            with open(file_path, 'r') as file:
+                numbers = []
+                for line_number, line in enumerate(file, start=1):
+                    stripped_line = line.strip()
+                    try:
+                        number = float(stripped_line)
+                        numbers.append(number)
+                    except ValueError:
+                        print(f"Warning: Could not convert line {line_number} to float: {stripped_line}")
 
-        with open(file_path, 'r') as file:
-            numbers = [float(line.strip()) for line in file.readlines()]
-            m2= [x**2 for x in numbers ]
-            m4= [x**4 for x in numbers ]
-            avg_m2 = calculate_mean (m2)
-            avg_m4 = calculate_mean (m4)
-            mm = calculate_mean(numbers)
-            U = avg_m4 / (3 * avg_m2**2)
-            cumulant.append (np.abs(U))
-            pseudo_magn.append(np.abs(mm))
+                if numbers:
+                    m2 = [x**2 for x in numbers]
+                    m4 = [x**4 for x in numbers]
+                    avg_m2 = calculate_mean(m2)
+                    avg_m4 = calculate_mean(m4)
+                    mm = calculate_mean(numbers)
+                    U = avg_m4 / (3 * avg_m2**2)
+                    cumulant.append(np.abs(U))
+                    pseudo_magn.append(np.abs(mm))
+
+        except FileNotFoundError:
+            print(f"Error: File {file_path} not found.")
 
     beta_array = np.array(betas)
 
-    
-    plt.plot(beta_array, cumulant, linestyle='-', label = f'L={l}', color=colors[i])
-
-    i = i+1
-
+    plt.plot(beta_array, cumulant, linestyle='-', label=f'L={l}', color=colors[i])
+    i += 1
 
 plt.xlabel(r'$\beta$')
-plt.ylabel('U')
-plt.title(f'Binder Cumulant e={e}')
+plt.ylabel('Binder Cumulant (U)')
+plt.title(f'Binder Cumulant for e={e}')
 plt.legend()
 plt.grid(True)
 plt.savefig(f'Binder_Cumulant_vs_beta_e={e}_K={K}.jpg')
-
-
 plt.show()
