8ec0343f6c2fafaeac89b1404a09be156b54317e
miriampicc <miriam.piccinini00@gmail.com>
2024-07-15 15:34:01 +0200

    only one day

--- launch_script/organize and run.sh
+++ launch_script/organize and run.sh
@@ -7,9 +7,9 @@ SCRIPT_DIR=${BASEDIR}/launch_script
 
 #############################################
 
-time_limit="5-0:00:00"
+time_limit="1-0:00:00"
 
-LLIST="8 12 16 20 24 "
+LLIST="8 12 16 20 24 32 "
 #32 40 48 64 96
 
 ################ Input Parameters for the Monte Carlo simulation #################
