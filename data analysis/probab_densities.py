5d6c290cb3cb9d01b729fff64b8ddc0e4d6f576f
miriampicc <miriam.piccinini00@gmail.com>
2024-08-14 14:21:40 +0200

    densities new link corr

--- data analysis/probab_densities.py
+++ data analysis/probab_densities.py
@@ -12,6 +12,8 @@ parser.add_argument('--e', type=float, help='e')
 parser.add_argument('--b_high', type=float, help='beta high')
 parser.add_argument('--b_low', type=float, help='beta low')
 parser.add_argument('--rank', type=int, help='rank')
+parser.add_argument('--a', type=float, help='a')
+
 
 # Parse the command-line arguments
 args = parser.parse_args()
@@ -23,6 +25,7 @@ e = args.e
 beta_high = args.b_high
 beta_low = args.b_low
 rank = args.rank
+a = args.a
 
 # Now you can use these values in your script
 print("L=", L)
@@ -31,6 +34,7 @@ print("e=", e)
 print("beta high=", beta_high)
 print("beta low=", beta_low)
 print("rank=", rank)
+print("a=", a)
 
 def calculate_std(data): 
     try:
@@ -101,7 +105,7 @@ for l in L:
         Psi2_r = []
 
 
-        file_path1 = f"/home/x_mirpi/Output_TBG/K_{K}_tdf2/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/Lattice_fin.txt'
+        file_path1 = f"/home/x_mirpi/Output_TBG/K_{K}_tdf2/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}_a{a}/beta_{n}" + '/Lattice_fin.txt'
         with open(file_path1, "r") as file:
             for line in file:
                 columns = line.strip().split()
