cd66c5b8101874cfc390daeee835cb814de1748d
miriampicc <miriam.piccinini00@gmail.com>
2024-08-16 12:10:46 +0200

    Rank histogram python 8

--- data analysis/Rank.py
+++ data analysis/Rank.py
@@ -58,7 +58,7 @@ plt.xlabel('Number')
 plt.ylabel('Occurrences')
 plt.xticks(range(0, 65, 5))
 plt.title('Histogram of Number Occurrences')
-plt.xticks(numbers)  # Ensure all numbers 0-63 are labeled
+#plt.xticks(numbers)  # Ensure all numbers 0-63 are labeled
 plt.grid(True)
 plt.savefig(f'Rank_e={e}_replica={beta}.jpg')
 
