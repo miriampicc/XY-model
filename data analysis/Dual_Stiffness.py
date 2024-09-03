ec3ef4c5c1db4941fa1d8b3463be423ec584cae6
miriampicc <miriam.piccinini00@gmail.com>
2024-09-03 10:29:15 +0200

    dual stiff

--- data analysis/Dual_Stiffness.py
+++ data analysis/Dual_Stiffness.py
@@ -12,6 +12,8 @@ parser.add_argument('--e', type=float, help='e')
 parser.add_argument('--b_high', type=float, help='beta high')
 parser.add_argument('--b_low', type=float, help='beta low')
 parser.add_argument('--rank', type=int, help='rank')
+parser.add_argument('--a', type=float, help='a')
+
 
 # Parse the command-line arguments
 args = parser.parse_args()
@@ -23,6 +25,8 @@ e = args.e
 beta_high = args.b_high
 beta_low = args.b_low
 rank = args.rank
+a = args.a
+
 
 # Now you can use these values in your script
 print("L=", L)
@@ -31,6 +35,7 @@ print("e=", e)
 print("beta high=", beta_high)
 print("beta low=", beta_low)
 print("rank=", rank)
+print("a=", a)
 
 
 # Create a dictionary to store the arrays
@@ -72,7 +77,7 @@ for l in L:
         
         temperatures.append(t)
 
-        file_path = f"/home/x_mirpi/Output_TBG/K_{K}/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}/beta_{n}" + '/Dual_Stiffness.txt'
+        file_path = f"/home/x_mirpi/Output_TBG/K_{K}/e_{e}/L{l}_K{K}_e{e}_bmin{beta_low}_bmax{beta_high}_a{a}/beta_{n}" + '/Dual_Stiffness.txt'
 
         with open(file_path, 'r') as file:
             numbers = [float(line.strip()) for line in file.readlines()]
