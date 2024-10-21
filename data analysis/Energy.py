2e39d2f6266e5505c9007d9a480806f7cf00ed7a
miriampicc <miriam.piccinini00@gmail.com>
2024-10-21 10:38:14 +0200

    new energy with a

--- data analysis/Energy.py
+++ data analysis/Energy.py
@@ -12,7 +12,7 @@ parser.add_argument('--e', type=float, help='e')
 parser.add_argument('--b_high', type=float, help='beta high')
 parser.add_argument('--b_low', type=float, help='beta low')
 parser.add_argument('--rank', type=int, help='rank')
-parser.add_argument('--a', type=float, help='rank')
+parser.add_argument('--a', type=float, help='bo')
 
 # Parse the command-line arguments
 args = parser.parse_args()
